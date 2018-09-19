68dd0f07f1635f53a1479e9819b6c90501b07c12
kitlingmui <kitlingmui@gmail.com>
2018-09-19 04:30:29 -0700

    Kit update 091919v2

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,23 +1,36 @@
-// When on click "let's cook" button, clear intro section, update search result section 
-$('#searchrecipe').on("click", function(){
-    $('#intro-section').empty();
-    $('#search-result-section').html(`<div class="card">  
-                                            <h3>Here are the list of recipe for you!</h3>
-                                      </div>`
-                                    )
+// When on click search recipe button, go to Recipe Page
+$('#search-recipe-btn').on("click", function(event){
+  event.preventDefault()
+  // Grabs user input
+  var searchfood = $("#food-input").val().trim();
+
+  // Clear input box
+  document.getElementById("food-input").value = "";
+  
+  var url = $(this).data('target');
+  location.replace(url);                             
 })
 
 
-// When on click search recipe button, clear intro seciotn, update search result section
-$('#search-recipe-btn').on("click", function(){
-    event.preventDefault()
+// Initialize Firebase for feedback section
+var config = {
+  apiKey: "AIzaSyDn_Bmwkve87XJmvSRGErlcXcwGWA8lJQM",
+  authDomain: "project1feedback.firebaseapp.com",
+  databaseURL: "https://project1feedback.firebaseio.com",
+  projectId: "project1feedback",
+  storageBucket: "",
+  messagingSenderId: "452695397144"
+};
+firebase.initializeApp(config);
 
-     // Grabs user input
-     var searchfood = $("#food-input").val().trim();
 
-    $('#intro-section').empty();
-    $('#search-result-section').html(`<div class="card">  
-                                            <h3>Here are the list of recipe on ${searchfood}!</h3>
-                                      </div>`
-                                    )
-})
+// Store all user feedback to database
+// to be updated
+
+
+// Display all user feedback on feedback page
+// to be updated
+
+
+
+
