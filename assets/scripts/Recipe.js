e242d8a6ff69b59b833d4bdff136c91b686bb054
hoang <hoangcao@mail.weber.edu>
2018-09-19 06:49:28 -0600

    add rating and time

--- assets/scripts/Recipe.js
+++ assets/scripts/Recipe.js
@@ -1,13 +1,14 @@
 
 
-var url = "https://www.food2fork.com/api/search?key=a1d99e1e0791dbbde08d37d85656f5f0&q="
+var url = "";
 $(".searchbutton").on("click", function()
 {
   event.preventDefault();
-  url = url + $(".search").val();
-  console.log(url) 
+  $(".RecipePicture").empty();
+  url = "https://api.yummly.com/v1/api/recipes?_app_id=fc9ece1f&_app_key=235229a497e877c3ca37916f4cdd111c" + "&q=" + $(".search").val() +"&requirePictures=true" + "&maxResult=10&start=10";
   getAPI();
 })
+
 
 function getAPI()
 {
@@ -17,7 +18,21 @@ function getAPI()
         })  
   .then(function(data)
     {
-      console.log(data);
+      for(i = 0; i<10; i++)
+      {
+        var imgDiv = $("<li>");
+        var imgURL = data.matches[i].imageUrlsBySize["90"]; 
+        var rate = $("<p>").text("Rating: " + data.matches[i].rating);  
+        var time = $("<p>").text("Time: " +data.matches[i].totalTimeInSeconds);  
+        console.log(data)
+        var image = $("<img>");
+        image.attr("src", imgURL);
+        image.attr("class", "icon");
+        imgDiv.append(rate);
+        imgDiv.append(time);
+        imgDiv.append(image);
+        $(".RecipePicture").prepend(imgDiv);
+      } 
     }) 
   .catch(function(err) {
       console.error('Fetch Error :-S', err);
